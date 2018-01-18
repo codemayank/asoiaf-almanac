f0e08b32ea9ce8e8577309c8143bf49c199a6596
codemayank <mayankyadav14@gmail.com>
2018-01-18 15:00:24 +0530

    change name of app

--- services/constants.js
+++ services/constants.js
@@ -3,7 +3,7 @@
 
 	angular.module('app')
 		.constant('constants', {
-			APP_TITLE : 'An Alamanac of Ice and Fire',
+			APP_TITLE : 'A Directory of Ice and Fire',
 			APP_DESCRIPTION : 'A source of information on the universe of ' + "'A song of ice and fire'.",
 			APP_VERSION : '1.0'
 
